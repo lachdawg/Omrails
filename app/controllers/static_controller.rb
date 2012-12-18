ad5fb6de20f602591769010011e0eddafa6dfacd
Brett Lacher <brett.lacher@gmail.com>
2012-12-18 16:43:57 -0500

    Initial commit

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def Pages
+  end
+
+  def home
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '9d872d8446cd142f7639f14b9cf13a341843db56157fccd4368d5127bd89bd87b8796236ac69ebb878f78bf10b3f318e1678e0790188e51ea188549fa44fb153'
